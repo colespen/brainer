62e625e9a5269787435ab5a58465bfd3a9daef0f
Spencer Cole <spencerlewiscole@gmail.com>
2025-08-20 18:20:50 -0700

    move deprecated 2d

---
+++ client/src/_2D/GameBoard.tsx
@@ -0,0 +1,47 @@
+import { useDispatch } from "react-redux";
+import { cardFlipped, cardFound, lossSet } from "../components/gameBoardSlice";
+import { GameBoardProps } from "../datatypes/proptypes";
+import GameCard from "./GameCard";
+
+function GameBoard({ gridN, cardData, gameBoard, ...rest }: GameBoardProps) {
+  const dispatch = useDispatch();
+
+  // turn clicked cards face up
+  const handleCardClick = (id: number) => {
+    if (!gameBoard.flippedCards.includes(id)) {
+      dispatch(cardFlipped(id));
+      if (cardData[id].isColor) {
+        // if correct card, cardsFound++
+        dispatch(cardFound());
+      } else {
+        // if wrong card, isLoss //   ***LOSS
+        dispatch(lossSet(true));
+      }
+    }
+  };
+
+  return (
+    <div
+      className="game-board"
+      style={{
+        gridTemplateRows: `repeat(${gridN}, 1fr)`,
+        gridTemplateColumns: `repeat(${gridN}, 1fr)`,
+      }}
+    >
+      {cardData.map((card) => (
+        <GameCard
+          key={card.id}
+          id={card.id}
+          color={card.color}
+          isColor={card.isColor}
+          isRevealed={gameBoard.isRevealed}
+          flippedCards={gameBoard.flippedCards}
+          handleCardClick={handleCardClick}
+          {...rest}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default GameBoard;
---
+++ client/src/_2D/GameCard.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { colorMap } from "../datatypes/colortypes";
+import { GameCardProps } from "../datatypes/proptypes";
+
+const GameCard = ({
+  id,
+  flippedCards,
+  color,
+  handleCardClick,
+  isLoss,
+  isWin,
+  isNewRound,
+  isRevealed,
+  isColor,
+}: GameCardProps) => {
+  const isFlipped = flippedCards.includes(id);
+  const [isClicked, setIsClicked] = useState(false);
+
+  useEffect(() => {
+    setIsClicked(false);
+  }, [isNewRound]);
+
+  const notFound = !isClicked && isColor;
+  const winColor = isWin && isColor ? colorMap.win : color;
+  const cardColor = isLoss && notFound ? colorMap.notFound : winColor;
+
+  return (
+    <button
+      className={
+        "btn board-card" +
+        (isFlipped ? " flipped" : "") +
+        (notFound ? " not-found" : "")
+      }
+      style={{
+        backgroundColor: isFlipped ? cardColor : colorMap.faceDown,
+      }}
+      onClick={() => {
+        handleCardClick(id);
+        setIsClicked(true);
+      }}
+      disabled={isRevealed || isLoss || isWin || isNewRound}
+    />
+  );
+};
+
+export default GameCard;
